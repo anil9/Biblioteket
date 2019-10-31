a7d8fa62da4def872f026c147504f31bfc881430
Andreas <anil9@kth.se>
2019-10-31 20:45:29 +0100

    start project

---
+++ src/main/java/com/biblioteket/start/BiblioteketApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteket.start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioteketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioteketApplication.class, args);
+	}
+
+}
