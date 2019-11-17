6d6a9aa5bce9d100fa4c7161df8bb974ef9de2f9
Andreas Nilsson <anil9@kth.se>
2019-11-17 21:27:58 +0100

    Webbsida med mvc-controller

--- src/main/java/com/biblioteket/Hjalpklass.java
+++ src/main/java/com/biblioteket/Hjalpklass.java
@@ -2,5 +2,5 @@ package com.biblioteket;
 
 public class Hjalpklass {
 
-    public static final String BAS_REPOSITORY_PATH = "D:\\downloads";
+    public static final String BAS_REPOSITORY_PATH = "/home/andreas/downloads";
 }
